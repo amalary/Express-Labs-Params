2438a10d582991bdf3d6cb6e98a3f1e2fb667f8e
Anthony Malary <amalary@bu.edu>
2022-04-20 11:45:31 -0700

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express'); 
+const { parse } = require('path/posix');
 
 const app = express();
 
@@ -16,6 +17,13 @@ app.get('/greetings/:name', (req,res) => {
     res.send(`Hello ${req.params.name} how have you been ? It's been so long since we last spoke.`)
 })
 
+
+// Tip Calculator 
+
+app.get('/tip/:total/:tipPercentage', (req,res) => {
+    const tipTotal = parseInt(req.params.total) * parseFloat(req.params.tipPercentage)
+    res.send(`The total for your bill is ${req.params.total} and the tip percentage is ${req.params.tipPercentage}. Your tip is ${tipTotal}`)
+})
 
 
 
